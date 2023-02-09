2e55fe53d6f919a8df36ab2ed9253895b3b7a66c
YMB <developer25132@gmail.com>
2023-02-09 11:46:20 +0300

    adding card

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey[300],
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -19,6 +19,65 @@ class MyApp extends StatelessWidget {
               CircleAvatar(
                 backgroundImage: AssetImage('images/image1.jpg'),
                 radius: 60.0,
+              ),
+              Text(
+                'Yohannis Mulugeta',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontFamily: 'Acme'
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontFamily: 'Anton'
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+                child: Divider(
+                  thickness: 1.0,
+                  color: Colors.white,
+                  indent: 80.0,
+                  endIndent: 80.0,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    '+251949772531',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.blueGrey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'Developer25132@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueGrey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
